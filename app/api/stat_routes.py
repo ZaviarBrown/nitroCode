bb6d32bfb2eb19ab8947c581fdcf3c6efade79c3
Zaviar Brown <brown.zaviar@gmail.com>
2021-07-12 04:40:55 -0700

    average cpm calculated

--- app/api/stat_routes.py
+++ app/api/stat_routes.py
@@ -1,9 +1,20 @@
 from flask import Blueprint
 from flask.globals import request
-from app.models import db, Stat
+from app.models import db, Stat, Race
 from flask_login import login_required, current_user
 
 stat_routes = Blueprint("stat", __name__)
+
+
+def findAvg(races):
+    cpms = []
+    for cpm in races:
+        cpms.append(cpm.cpm)
+    totalCpm = 0
+    for cpm in cpms:
+        totalCpm += cpm
+    avgCpm = totalCpm / len(cpms)
+    return avgCpm
 
 
 @stat_routes.route('/<int:id>')
@@ -32,8 +43,11 @@ def create_stat():
 def update_stat():
     cpm = request.json
     stat = Stat.query.filter(Stat.userId == current_user.id).first()
+    races = Race.query.filter(Race.userId == current_user.id).all()
+    averageCpm = findAvg(races)
     if cpm > stat.highestCpm:
         stat.highestCpm = cpm
+    stat.averageCpm = averageCpm
     stat.races = stat.races + 1
     db.session.commit()
     return {"cpm": cpm}
